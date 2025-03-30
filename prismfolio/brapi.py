ff6a11898f1518bd9ae19cc2d6efd46b9ae0e9bc
hugocpolos <hugo.cpolos@gmail.com>
2025-03-30 20:26:08 -0300

    Function to get price earnings of a stock
    Signed-off-by: hugocpolos <hugo.cpolos@gmail.com>

--- prismfolio/brapi.py
+++ prismfolio/brapi.py
@@ -15,3 +15,18 @@ def get_current_stock_price(stock_code):
 
     data = response.json()
     return data.get('results')[0].get('regularMarketPrice')
+
+
+def get_price_earning(stock_code):
+    url = "https://brapi.dev/api/quote/{}".format(stock_code.upper())
+    params = {
+        'token': os.getenv('STOCK_API_KEY'),
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        raise requests.HTTPError(f"Request failed with status code {response.status_code}."
+                                 f"\n\t{response.json()}")
+
+    data = response.json()
+    return data.get('results')[0].get('priceEarnings')
