246fff1d1d090d4634588c1c15c2d56adec511e3
hugocpolos <hugo.cpolos@gmail.com>
2025-03-30 22:07:22 -0300

    Fix some visual errors and pylint issues on app.py
    Signed-off-by: hugocpolos <hugo.cpolos@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from prismfolio.wallet import Wallet
-from prismfolio.investmentsuggestion import WalletInvestmentSuggestion
-from prismfolio import brapi
-import json
 import argparse
+import json
 import logging
+
+from prismfolio import brapi
+from prismfolio.investmentsuggestion import WalletInvestmentSuggestion
+from prismfolio.wallet import Wallet
 
 
 def argument_parser():
@@ -14,21 +15,24 @@ def argument_parser():
     return parser.parse_args()
 
 
-def dry_run_function(*args):
+def dry_run_function(_):
     return 1.0
 
 
 def display_suggestion(suggestion: WalletInvestmentSuggestion):
+    _display_group_layout = "{}: R$ {:5.2f}"
+    _display_asset_layout = "\t[{}] R$ {:5.2f} Buy {} shares"
+
     for group in suggestion:
         if group.get_suggested_investment() == 0:
             continue
 
-        print("{}: R% {:5.2f}".format(group.get_investment_group().get_name(),
-                                      group.get_suggested_investment()))
+        print(_display_group_layout.format(group.get_investment_group().get_name(),
+                                           group.get_suggested_investment()))
         for asset in group:
             if asset.get_suggested_investment() == 0:
                 continue
-            print("\t[{}] R$ {:5.2f} Buy {} shares".format(
+            print(_display_asset_layout.format(
                 asset.get_asset().get_code(),
                 asset.get_suggested_investment(),
                 asset.get_suggested_shares_buying()))
@@ -37,13 +41,13 @@ def display_suggestion(suggestion: WalletInvestmentSuggestion):
 def main():
     args = argument_parser()
     stock_price_acquisition_function = brapi.get_current_stock_price
-    stock_price_earning_acquisitiong_function = brapi.get_price_eanings
+    stock_price_earning_acquisitiong_function = brapi.get_price_earning
 
     if args.dry_run:
         stock_price_acquisition_function = dry_run_function
         stock_price_earning_acquisitiong_function = dry_run_function
 
-    with open(args.input_data) as fp:
+    with open(args.input_data, encoding="utf-8") as fp:
         input_dict = json.load(fp)
 
     try:
